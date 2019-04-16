65618a4be0a371c5c610a7eeddd1e7af916a583c
Roushan Sourav <roushansourav@outlook.com>
2019-04-16 14:17:18 +0530

    Add project files.

---
+++ highest_feedback/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace highest_feedback
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            int[] b = new int[n];
+            int[] c = new int[2 * n];
+            for (int i = 0; i < n; i++)
+                a[i] = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+                b[i] = int.Parse(Console.ReadLine());
+            int k = 0;
+
+            for(int i=0;i<n-1;i=i+2)
+            {
+                bool match = false;
+                for(int j=0;j<n-1;j=j+2)
+                {
+                    if(a[i]==b[j])
+                    {
+                        if(a[i+1]>b[j+1])
+                        {
+                            c[k] = a[i];
+                            c[k + 1] = a[i + 1];
+                            k = k + 2;
+                            b[j] = 0;
+                            b[j + 1] = 0;
+                            match = true;
+                        }
+                        else
+                        {
+                            c[k] = b[j];
+                            c[k + 1] = b[j + 1];
+                            b[j] = 0;b[j+ 1] = 0;
+                            k = k + 2;
+                            match = true;
+                        }
+                    }
+                }
+                if(!match)
+                {
+                    c[k] = a[i];
+                    c[k + 1] = a[i + 1];
+                    k = k + 2;
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (b[i] != 0)
+                {
+                    c[k] = b[i];
+                    k++;
+                }
+            }
+            for (int i = 0; c[i] != 0&&i<k; i++)
+                Console.Write(c[i]+" ");
+            Console.WriteLine();
+            for (int i = 0; i < n; i++)
+                Console.Write(b[i] + " ");
+        }
+    }
+}
---
+++ highest_feedback/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("highest_feedback")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("highest_feedback")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dd778167-9464-4f1a-95a2-de29494c1b72")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
